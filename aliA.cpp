0b89f5f434a1a222163b7897496a369d145181f8
Student <student@lab-240-022.cs.csubak.edu>
2019-09-25 10:38:36 -0700

    spaces fixed

--- aliA.cpp
+++ aliA.cpp
@@ -4,11 +4,10 @@
 
 
 void showCredit() {
-	    Rect n;
-		n.bot = 400;
-	    n.left = 280;
-		n.center = 0;
-		ggprint8b(&n, 16, 0x00ffffff, "Ali Alherz");
+    Rect n;
 
-	
+    n.bot = 400;
+    n.left = 280;
+    n.center = 0;
+    ggprint8b(&n, 16, 0x00ffffff, "Ali Alherz");
 }
