e02b376ef611c5235d58f9fa9ecc41be0fc433aa
aalherz86 <aalherz86@gmail.com>
2019-10-13 17:00:18 -0700

    implimenting highscore system

--- aliA.cpp
+++ aliA.cpp
@@ -1,9 +1,4 @@
 // Ali Alherz
-// 
-//
-// 
-//
-//
 /* the debugging steps:
  * 1. recognize that a bug exists.
  * 2. isolate the bad code.
@@ -11,22 +6,75 @@
  * 4. test the source code.
  */
 
-
 #include "fonts.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm> 
 
+using namespace std;
 
-void showCredit() {
-    Rect n;
-
-    n.bot = 400;
-    n.left = 280;
-    n.center = 0;
-    ggprint8b(&n, 16, 0x00ffffff, "Ali Alherz");
-}
- 
-
-void phpInfo()
+// getting current highscores
+vector<int> getHighscore ()
 {
-        char hostname[255] = "odin.cs.csub.edu";
-        char pagename[255] = "/~aalherz/3350/lab3/lab3.php";
+	ifstream myfile("highscores.txt");
+	vector<int> scores (5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		myfile >> scores[i];
+	}
+	return scores;
+}
+
+// finding the minimum score
+int findMin(vector<int> myVector) 
+{     
+	// Initialize minimum element 
+	int min = myVector[0]; 
+
+	// Traverse vector elements  
+	for (size_t i = 1; i < myVector.size(); i++) 
+		if (myVector[i] < min) 
+			min = myVector[i]; 
+
+	return min; 
+} 
+
+// save scores to txt file
+void writeScores(int score, vector<int> scores)
+{
+	int last = findMin(scores); 
+
+	if (score > last)
+		scores.push_back (score);
+
+	// sorting in descending order
+	sort(scores.begin(), scores.end(), greater<int>());
+
+	// open txt file 
+	ofstream myfile ("highscores.txt");
+
+	if (myfile.is_open()) {
+		for (auto x : scores) {
+			myfile << x <<"\n";
+		}
+		myfile.close();
+	}
+}
+
+void showCredit() 
+{
+	Rect n;
+
+	n.bot = 400;
+	n.left = 280;
+	n.center = 0;
+	ggprint8b(&n, 16, 0x00ffffff, "Ali Alherz");
+}
+
+void phpInfo ()
+{
+	char hostname[255] = "odin.cs.csub.edu";
+	char pagename[255] = "/~aalherz/3350/lab3/lab3.php";
 }
