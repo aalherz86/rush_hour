cfbdee7090875ac135ddaa85439fb51f79f59165
Mohanad333 <malgoraibi@csub.edu>
2019-11-03 12:31:08 -0800

    Moving my functions to a new framework

--- mohanadA.cpp
+++ mohanadA.cpp
@@ -3,6 +3,7 @@
 //Second Modification on the main file (car.cpp): Display the Highest scores on screen.
 //Third Modification: adding the function from lab3 and call it in hussainJ file
 //Fourth Modification: created a function called displayHighscores to get 5 scores on screen.
+//Fifth Modification: Moving and fixing all my functions to a new framework.
 /* The Debug steps:
    1-Identify (checking) is to find the debugging.
    2-Isolate is to separate the buggy code with other healthy code
@@ -11,7 +12,6 @@
    */
 
 #include "fonts.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -24,14 +24,16 @@
 #include <openssl/pem.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+
 using namespace std;
+
 extern vector<int> getHighscore ();
 
 void showMcredit() 
 {
 	Rect n;
-	n.bot = 360;
-	n.left = 280;
+	n.bot = 580;
+	n.left = 820;
 	n.center = 0;
 	ggprint8b(&n, 16, 0x00fffff00, "Mohanad Algoraibi");
 
@@ -60,21 +62,21 @@ void displayHighscores()
 	scores = getHighscore();
 	Rect n[6];
 
-	n[0].bot = 400;
-	n[0].left = 280;
+	n[0].bot = 720;
+	n[0].left = 820;
 	n[0].center = 0;
-	ggprint8b(&n[0], 16, 0x00000000, "Highscores");
-	n[1].bot = 380;
-	n[1].left = 280;
+	ggprint8b(&n[0], 16, 0x00ffffff, "Highscores");
+	n[1].bot = 700;
+	n[1].left = 820;
 	n[1].center = 0;
 
-	n[2].bot = 380;
-	n[2].left = 260;
+	n[2].bot = 700;
+	n[2].left = 800;
 	n[2].center = 0;	
 
 	for (size_t i = 0; i < 5; i++)
 	{
-		ggprint8b(&n[1], 16, 0x00fe3e3e, " %i", scores[i]);
+		ggprint8b(&n[1], 16, 0x00ffffff, " %i", scores[i]);
 	}
 	for (size_t i = 1; i < 6; i++)
 	{
