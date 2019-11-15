c3187314d7923000a236f40d725e30114441cf27
Student <student@lab-240-021.cs.csubak.edu>
2019-11-15 10:52:27 -0800

    Some functions added on Fridday

--- hussainJ.cpp
+++ hussainJ.cpp
@@ -25,13 +25,15 @@
 #include <openssl/pem.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+// Added new Library for the game timer
+#include <time.h>
 
 //void set_to_non_blocking(const int sock);
 // Global Area
 #define PORT 443
 #define USERAGENT "CMPS-3350"
 const int MAX_READ_ERRORS = 100;
-
+// This function to display my name on the screen.
 void displayName() {
 	Rect n;
 	n.bot = 560;
@@ -50,4 +52,22 @@ void drawImage(GLuint texture, int xres, int yres)
 	glTexCoord2f(1.0f, 0.0f); glVertex2i(xres, yres);
 	glTexCoord2f(1.0f, 1.0f); glVertex2i(xres, 0);
 	glEnd();
+}
+// This is my Fridday code.
+// I have added a functoin that to display a timer by seconds and store the Game timer for the user on the screen
+//
+void GameTimer() {
+    time_t sec;
+    // To store the timing in the score file:
+    time(&sec);
+    cout << ("Timer Seconds: ", sec);
+}
+// Display seconds on the screen to let the user see their timing on the game.
+// Domentoin needs to be updated
+void displaySeconds() {
+    Rect n;
+    n.bot = 500;
+    n.left = 460;
+    n.center = 0;
+    ggprint8b(&n, 10, 0x00fffff, "Timer: ");
 }
