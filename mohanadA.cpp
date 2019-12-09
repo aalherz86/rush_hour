5d2155125b27d948f08d4898d089c90a33ab3bd2
Mohanad333 <42781102+Mohanad333@users.noreply.github.com>
2019-12-09 01:07:42 -0800

    adding credit screen

--- mohanadA.cpp
+++ mohanadA.cpp
@@ -14,6 +14,7 @@
 // seventh Modification: creating mohanadA Header.
 
 #include "fonts.h"
+#include <iostream>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -29,12 +30,16 @@
 #include "mohanadA.h"
 #include <GL/glu.h>
 #include "Image.h"
-
+#include "aliA.h"
+#include <GL/gl.h>
+#include <GL/glx.h>
 
 
 using namespace std;
 
 extern vector<int> getHighscore ();
+void credits();
+Play pl;
 
 void showMcredit() 
 {
@@ -42,9 +47,84 @@ void showMcredit()
 	n.bot = 580;
 	n.left = 820;
 	n.center = 0;
-	ggprint8b(&n, 16, 0x00fffff00, "Mohanad Algoraibi");
+	ggprint8b(&n, 16, 0x00ffffff, "Mohanad Algoraibi");
 
 
+}
+Button btn[MAXBUTTONS];
+void backBtn_init(void)
+{
+
+    btn[0].r.width = BUTTON_WIDTH +30;
+    btn[0].r.height = BUTTON_HEIGHT;
+    btn[0].r.left = 1050 - btn[0].r.width/2;
+    btn[0].r.bot = BUTTON_B -280;
+    btn[0].r.right =
+            btn[0].r.left + btn[0].r.width;
+    btn[0].r.top =
+            btn[0].r.bot + btn[0].r.height;
+    btn[0].r.centerx =
+            (btn[0].r.left + btn[0].r.right) / 2;
+    btn[0].r.centery =
+            (btn[0].r.bot + btn[0].r.top) / 2;
+    strcpy(btn[0].text, "BACK");
+    btn[0].down = 0;
+    btn[0].click = 0;
+    btn[0].color[0] = 0.0f;
+    btn[0].color[1] = 0.0f;
+    btn[0].color[2] = 0.0f;
+    btn[0].dcolor[0] = btn[0].color[0] * 0.5f;
+    btn[0].dcolor[1] = btn[0].color[1] * 0.5f;
+    btn[0].dcolor[2] = btn[0].color[2] * 0.5f;
+    btn[0].text_color = 0x00ffffff;
+
+}
+
+void btnRender(void)
+{
+
+    //add render function to render button
+    for (int i = 0; i < MAXBUTTONS; i++) {
+        if (btn[i].over) {
+            //create the highlight color of button
+            glColor3f(1.0f, 1.0f, 1.0f);
+            //draw a highlight around the button
+            glLineWidth(2);
+            glBegin(GL_LINE_LOOP);
+            glVertex2i(btn[i].r.left-2,  btn[i].r.bot-2);
+            glVertex2i(btn[i].r.left-2,  btn[i].r.top+2);
+            glVertex2i(btn[i].r.right+2, btn[i].r.top+2);
+            glVertex2i(btn[i].r.right+2, btn[i].r.bot-2);
+            glVertex2i(btn[i].r.left-2,  btn[i].r.bot-2);
+            glEnd();
+            glLineWidth(2);
+            btn[i].text_color = 0x00fff333;
+
+        }
+        if (btn[MAXBUTTONS - 1].over)
+            btn[i].text_color = 0x00ff0000;
+
+        glBegin(GL_QUADS);
+        if (btn[i].down) {
+            glColor3fv(btn[i].dcolor);
+        } else {
+            glColor3fv(btn[i].color);
+        }
+        glVertex2i(btn[i].r.left,  btn[i].r.bot);
+        glVertex2i(btn[i].r.left,  btn[i].r.top);
+        glVertex2i(btn[i].r.right, btn[i].r.top);
+        glVertex2i(btn[i].r.right, btn[i].r.bot);
+        glEnd();
+        Rect r;
+        r.left = btn[i].r.centerx;
+        r.bot  = btn[i].r.centery-8;
+        r.center = 1;
+        if (btn[i].down) {
+            ggprint16(&r, 0, btn[i].text_color, "Pressed!");
+        } else {
+            ggprint16(&r, 0, btn[i].text_color, btn[i].text);
+        }
+    }
 }
 
 void set_to_non_blocking(const int sock)
@@ -69,27 +149,23 @@ void displayHighscores()
 	scores = getHighscore();
 	Rect n[6];
 
-	n[0].bot = 720;
-	n[0].left = 820;
-	n[0].center = 0;
-	ggprint8b(&n[0], 16, 0x00ffffff, "Highscores");
-	n[1].bot = 700;
-	n[1].left = 820;
+	n[1].bot = 600;
+	n[1].left = 660;
 	n[1].center = 0;
 
-	n[2].bot = 700;
-	n[2].left = 800;
+	n[2].bot = 600;
+	n[2].left = 610;
 	n[2].center = 0;	
 
 	for (size_t i = 0; i < 5; i++) {
-		ggprint8b(&n[1], 16, 0x00ffffff, " %i", scores[i]);
+		ggprint16(&n[1], 16, 0x00ffffff, " %i", scores[i]);
 	}
 	for (size_t i = 1; i < 6; i++) {
-		ggprint8b(&n[2], 16, 0x00ffffff, " %i - ", i);
+		ggprint16(&n[2], 16, 0x00ffffff, " %i - ", i);
 	}
 }
 
-// This is my Friday code.
+// This is my Friday code
 void drawVehicle(int vehicleX, int vehicleY)
 {
     glPointSize(10.0);
@@ -131,35 +207,164 @@ Image * title;
 GLuint titleTex;
 void initCreditScreen()
 {
-
     title = new Image("images/credit.png");
     glGenTextures( 1, &titleTex);
     glBindTexture( GL_TEXTURE_2D, titleTex);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
     glTexImage2D( GL_TEXTURE_2D, 0, 3, title->width, title->height, 0, GL_RGB,
                   GL_UNSIGNED_BYTE, title->data);
-    glBindTexture( GL_TEXTURE_2D, 0);
+}
+
+void drawCredit(int xres, int yres)
+{
+
+    glPushMatrix();
+    drawImage(titleTex, xres, yres);
+    glPopMatrix();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    backBtn_init();
+    btnRender();
+    credits();
 
 }
 
-void drawCredit(int xres, int yres) {
+void checkMouse(int action, Play& p)
+{
+    if (p.gameState == CREDITS) {
+        if (action == 1) {
+            for (int i=0; i<MAXBUTTONS; i++) {
+                if (btn[i].over) {
+                    btn[i].down = 1;
+                    btn[i].click = 1;
+                    if (i == 0) {
+                        p.gameState = MENU ;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
 
-    float w;
-    float h;
+void checkBtn(XEvent *e, Play& p)
+{
+    static int savex = 0;
+    static int savey = 0;
+    int x,y,i;
+    int lbutton=0;
+    int rbutton=0;
+    //when mouse button is release
+    if (e->type == ButtonRelease) {
+        checkMouse(2, p);
+        return;
+    }
+    if (e->type == ButtonPress) {
+        //when mouse left button is pressed
+        if (e->xbutton.button == 1) {
+            lbutton = 1;
+        }
+        //when mouse right button is pressed
+        if (e->xbutton.button == 3) {
+            rbutton = 1;
+        }
+    }
+    x = e->xbutton.x;
+    y = e->xbutton.y;
+    y = 1000-y;
+    //check to see if mouse move either in x or y direction
+    if (savex != e->xbutton.x || savey != e->xbutton.y) {
+        savex=e->xbutton.x;
+        savey=e->xbutton.y;
 
-    glPushMatrix();
-    w = 250;
-    h = 150;
-    glTranslatef(xres/2, yres/2 + 200, 0.0f);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glBindTexture(GL_TEXTURE_2D, titleTex);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 1);glVertex2f(-w, -h);
-    glTexCoord2f(0.00, 0.00);glVertex2f(-w,  h);
-    glTexCoord2f(1.00, 0.00);glVertex2f( w,  h);
-    glTexCoord2f(1.00, 1);glVertex2f( w, -h);
-    glEnd();
+    }
+    if (x == savex && y == savey)
+        return;
+
+    savex=x;
+    savey=y;
+    for (i=0; i<MAXBUTTONS; i++) {
+        btn[i].over=0;
+        btn[i].down=0;
+
+        if (x >= btn[i].r.left &&
+            x <= btn[i].r.right &&
+            y >= btn[i].r.bot &&
+            y <= btn[i].r.top) {
+            btn[i].over=1;
+            break;
+        }
+    }
+    if (lbutton)
+        checkMouse(1, p);
+    if (rbutton)
+        checkMouse(2, p);
+
+}
+
+void creditInit(GLuint tex, Image& img)
+{
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, 3,
+                 img.width, img.height,
+                 0, GL_RGB, GL_UNSIGNED_BYTE, img.data);
+
+}
+
+void showName(Rect& r, int bot, int left, const char* txt)
+{
+    r.bot = bot;
+    r.left = left;
+    r.center = 0;
+    ggprint16(&r, 0, 0x00ffffff, txt);
+}
+
+void credits()
+{
+    Rect r[12];
+    r[0].bot = 650;
+    r[0].left = 660;
+    r[0].center = 0;
+    ggprint16(&r[0], 0, 0x00fff222, "Menu by: ");
+    showName(r[6], r[0].bot -40 , 660, "Hussain Jafri");
+
+    r[1].bot = 570;
+    r[1].left = 660;
+    r[1].center = 0;
+    ggprint16(&r[1], 0, 0x00fff222, "Credit by: ");
+    showName(r[7], r[1].bot -40 , 660, "Mohanad Algoraibi");
+
+    r[2].bot = 490;
+    r[2].left = 660;
+    r[2].center = 0;
+    ggprint16(&r[2], 0, 0x00fff222, "Tiles System by: ");
+    showName(r[8], r[2].bot -40 , 660, "Ali Alherz");
+
+
+    r[3].bot = 410;
+    r[3].left = 660;
+    r[3].center = 0;
+    ggprint16(&r[3], 0, 0x00fff222, "Graphics by: ");
+    showName(r[9], r[3].bot -40 , 660, "Mohanad Algoraibi");
+
+
+    r[5].bot = 330;
+    r[5].left = 660;
+    r[5].center = 0;
+    ggprint16(&r[5], 0, 0x00fff222, "High score system by: ");
+    showName(r[10], r[5].bot -40 , 660, "Ali Alherz");
+
+
+    r[4].bot = 250;
+    r[4].left = 660;
+    r[4].center = 0;
+    ggprint16(&r[4], 0, 0x00fff222, "Physics by: ");
+    showName(r[11], r[4].bot -40 , 660, "Hussain Jafri");
+
+
 }
 
 
--- mohanadA.h
+++ mohanadA.h
@@ -3,7 +3,40 @@
 #ifndef MOHANAD_H
 #define MOHANAD_H
 
+#include "fonts.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <vector>
+#include "mohanadA.h"
+#include <GL/glu.h>
+#include "Image.h"
+#include "aliA.h"
+#include <GL/gl.h>
+#include <GL/glx.h>
+//-------------------------------------------------
+#define BUTTON_WIDTH 50
+#define BUTTON_HEIGHT 50
+#define BUTTON_B 400
+// Defining 4 buttons
+#define MAXBUTTONS 4
+//------------------------
 
+extern void drawImage(GLuint, int, int);
 
+typedef struct t_button {
+    Rect r;
+    char text[32];
+    int over;
+    int down;
+    int click;
+    float color[3];
+    float dcolor[3];
+    unsigned int text_color;
+    t_button() { over = false; }
+}Button;
 
 #endif
